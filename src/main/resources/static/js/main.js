efabf93cedf78ce646f26bce652d6a3a341168c3
TienDat <tadat290903@gmail.com>
2023-12-19 20:16:03 +0700

    update home UI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -33,4 +33,29 @@ function showModalInCustomer(){
     });
 }
 
+function showModalInHome(id){
+    document.addEventListener('DOMContentLoaded', function () {
+        // add-product
+        console.log(id);
+        var openModalButton = document.querySelector('#'+id);
+        var userIdInput = document.getElementById('product_id');
+
+        openModalButton.addEventListener('click', function () {
+            var userId = this.getAttribute('data-id');
+            userIdInput.value = userId;
+        });
+    });
+
+    // remove product
+    document.addEventListener('DOMContentLoaded', function () {
+        var openModalButton = document.querySelector('#'+id);
+        var userIdInput = document.getElementById('productID');
+
+        openModalButton.addEventListener('click', function () {
+            var userId = this.getAttribute('data-id');
+            userIdInput.value = userId;
+        });
+    });
+}
+
 
